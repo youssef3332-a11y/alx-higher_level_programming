1da11eacb6e139a5ea04daebbd20a8d22abf30e3
youssef3332-a11y <youssef17102003@gmail.com>
2024-04-21 11:26:27 +0100

    new file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            test = i
+            test = i + 1
         except IndexError:
             break
     print("")
