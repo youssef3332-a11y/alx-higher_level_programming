f388dd2002f6aae029ede72a118218e6336d0e8f
youssef3332-a11y <youssef17102003@gmail.com>
2024-04-27 19:29:15 +0100

    new file

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/user/bin/python
+import sys
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as ex:
+        sys.stderr.write("Exception: {}\n".format(ex))
+        return None
