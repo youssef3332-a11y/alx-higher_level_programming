39082c9f77361cb9be27a7c212e48a654f292999
youssef3332-a11y <youssef17102003@gmail.com>
2024-04-29 14:21:24 +0100

    new file

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""nothing this time"""
+
+
+class Square:
+    """paint it black"""
+    def __init__(self, size=0):
+        """ __init__"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """ area methode"""
+        return self.__size ** 2
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            line = "#"*self.__size + "\n"
+            print(line * self.__size, end="")
