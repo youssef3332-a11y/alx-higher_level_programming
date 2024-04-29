72fc36b98116c681b7f60c16b969704747a3c5ae
youssef3332-a11y <youssef17102003@gmail.com>
2024-04-29 13:02:07 +0100

    new file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""discover classes """
+
+
+class Square:
+   """class Square"""
+   def __init__(self, size):
+      self.__size = size
--- 0x06-python-classes/testing.py
+++ 0x06-python-classes/testing.py
@@ -1,5 +1,5 @@
 
 p=__import__('0-square').Square
-my_square = p()
+my_square =p()
 print(type(my_square))
 print(my_square.__dict__)
