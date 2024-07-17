bc8ac942cddc7fc04d9eb173fd04cea313b8a0a8
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-17 19:22:23 +0100

    new file

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -88,15 +88,3 @@ class Rectangle:
             return (self.__width * 2) + (self.__height * 2)
     def __str__(self):
         return (("#" * self.width + "\n") * (self.height - 1) + "#" * self.width)
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
