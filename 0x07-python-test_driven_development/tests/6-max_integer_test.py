f58623ff7731877113a7422212854a82e464cfd2
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-17 12:04:48 +0100

    new file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -15,4 +15,12 @@ class TestMaxInteger(unittest.TestCase):
         self.assertIsNone(max_integer([]))
     def test_isCorret(self):
         self.assertEqual(max_integer([32,56,4,3.5]), 56)
+        self.assertEqual(max_integer([32]), 32)
+    def test_max_where(self):
+        self.assertEqual(max_integer([32, 4, 56, 3, 6]), 56)
+        self.assertEqual(max_integer([56, 4, 5, 3, 6]), 56)
+        self.assertEqual(max_integer([32, 4, 5, 3, 56]), 56)
+    def test_max_negatif(self):
+        self.assertEqual(max_integer([-32, -4, -56, -3, -6]), -3)
+        self.assertEqual(max_integer([32, -4, 56, 3, 6]), 56)
     
