e420eec30150ad9c1a1910a96eda5916cf30802f
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-20 08:24:29 +0100

    new file

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,14 +6,3 @@ def lookup(obj):
     dir function
     """
     return dir(obj)
-class MyClass1(object):
-    pass
-
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
