4b69516ed88f829ad23f25cede4ca096d7276e39
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-23 15:29:23 +0100

    Update write_file function to use 'with' statement for file handling

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@ module
 """
 def write_file(filename="", text=""):
     """Writes a string to a text file (UTF8) and returns the number of characters written."""
-    with open(filename, 'w', encoding='utf-8'):
+    with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
