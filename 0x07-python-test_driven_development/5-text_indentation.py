82a6f61b5359fd4f96fe8b0f99af2d4ec40547f4
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-23 07:30:14 +0100

    new file

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+# !usr/bin/python3
+"""module:
+metriser les tests
+"""
+def text_indentation(text):
+    """module:
+    Args: text
+    Raises: TypeError
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        print(text[i], end='')
+        if text[i] in ('.', '?', ':'):
+            print("\n")
+            if text[i + 1] == ' ':
+                i+=1
+        i+=1
