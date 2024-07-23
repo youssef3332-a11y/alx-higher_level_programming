abc852d5e13274ef81417b6ea667a4f241639239
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-23 15:50:40 +0100

    Update from_json_string function to use json.loads for parsing a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bim/python3
+"""
+module
+..
+"""
+import json
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string."""
+    return json.loads(my_str)
