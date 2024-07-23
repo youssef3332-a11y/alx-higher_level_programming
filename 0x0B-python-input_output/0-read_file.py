771da0bd82b0d82eea35ae1caa2b7a322498c53e
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-23 15:05:40 +0100

    new file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
 module
 .
@@ -6,4 +6,4 @@ module
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its contents to stdout."""
     with open(filename, encoding="utf-8") as file:
-    print(file.read())
+        print(file.read(), end='')
