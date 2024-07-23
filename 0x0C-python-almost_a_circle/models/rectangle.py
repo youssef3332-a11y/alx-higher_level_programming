a8553308d70ecc6aa426aca7545713c65cb613dc
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-23 20:42:37 +0100

    chore: Add input validation for width, height, x, and y in Rectangle class constructor

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,6 +14,18 @@ class Rectangle(Base):
         """
         function discreption
         """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__width = width
         self.__height = height
         self.x = x
