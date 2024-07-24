24bb251f13108f0c4d171ad38650c1c42cb2ee37
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-24 16:01:45 +0100

    refactor: Update Base class to include a to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,9 +3,13 @@
 module
 ..
 """
+import json
+
+
 class Base:
     """Base class."""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """Initializes an instance."""
         if id is not None:
@@ -16,7 +20,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries."""
-        import json
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
