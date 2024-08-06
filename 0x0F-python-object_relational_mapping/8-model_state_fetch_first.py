2b73e94c9b56737cb12128cb7930b1576a48b10e
youssef3332-a11y <youssef17102003@gmail.com>
2024-08-06 22:26:40 +0100

    a interesting task

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script to create the 'states' table in the database.
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}', pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.id == 1).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Not found")
