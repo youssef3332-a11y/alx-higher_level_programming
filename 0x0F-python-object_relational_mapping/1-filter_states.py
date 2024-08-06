5a75d8ef09195cea1f10f5ca942c71ca1c0547bd
youssef3332-a11y <youssef17102003@gmail.com>
2024-08-06 18:51:02 +0100

    new file

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""this module"""
+import MySQLdb
+import sys
+
+
+def list_creates(u, p, d):
+    """ this is the description"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=u, passwd=p, db=d)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    list_creates(username, password, db)
