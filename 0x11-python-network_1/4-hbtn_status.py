ee9d21053d6907a5c2b00bd27cdb28c2265a6d6e
youssef3332-a11y <youssef17102003@gmail.com>
2024-08-16 11:13:12 +0100

    new file

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/puthon3
+""" his case """
+import requests
+
+
+def fetch_status():
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
+
+
+if __name__ == "__main__":
+    fetch_status()
